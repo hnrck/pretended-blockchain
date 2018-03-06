521d14f083705c280cddc8a07aac1ba4c906ef43
Henrick Deschamps <henrick.deschamps@gmail.com>
2018-03-06 18:18:24 +0100

    Adding the block implementation
    - Adding the block implementation src.
    - Adding the block tst.
    - Enabling block test in cabal file.

--- src/Blockchain/Block.hs
+++ src/Blockchain/Block.hs
@@ -16,10 +16,15 @@ module Blockchain.Block
   , mineBlock
   ) where
 
+import qualified Crypto.Hash.SHA256           as SHA256
+import           Data.ByteString              (ByteString)
 import           Data.Maybe                   (Maybe (..))
 
-import           Blockchain.Block.Header      (Header (..))
-import           Blockchain.Block.Transaction (Transaction (..))
+import           Blockchain.Block.Header      (Header (..), genesisHeader,
+                                               headerContext, newHeader,
+                                               setHash)
+import           Blockchain.Block.Transaction (Transaction (..),
+                                               transactionContext)
 
 -- | The blockchain's block. A block can be printed or tested for equality. 
 -- The header is computed at creation, and the transaction is not mandatory.
@@ -28,12 +33,25 @@ data Block = Block
   , transaction :: Maybe Transaction
   } deriving (Show, Eq)
 
+-- | Generate the context of the block, from it header and transaction's
+-- contexts.
+blockContext :: Block -> [ByteString]
+blockContext (Block hdr tx) = headerContext hdr ++ transactionContext tx
+
+-- | Compute the block hash from its context.
+computeHash :: Block -> ByteString
+computeHash = SHA256.finalize . SHA256.updates SHA256.init . blockContext
+
+-- | Hash a block.
+hashBlock :: Block -> Block
+hashBlock (Block hdr tx) = Block (setHash hdr (computeHash (Block hdr tx))) tx
+
 -- | Generate the first block of a blockchain. The transaction is not mandatory
 -- and can be left to nothing.
 genesisBlock :: Maybe Transaction -> Block
-genesisBlock = undefined
+genesisBlock = hashBlock . Block genesisHeader
 
 -- | Mine a new block using a previous one. The transaction is not mandatory
 -- and can be left to nothing.
 mineBlock :: Block -> Maybe Transaction -> Block
-mineBlock = undefined
+mineBlock (Block (Header _ ts h) _) = hashBlock . Block (newHeader h (ts + 1))
--- tst/unit/BlockSpec.hs
+++ tst/unit/BlockSpec.hs
@@ -1,7 +1,43 @@
 module BlockSpec where
 
-import           Test.Hspec
-import           Test.QuickCheck
+import           Data.ByteString.Char8        (unpack)
+import           Test.Hspec                   (Spec (..), describe, it,
+                                               shouldBe)
+import           Test.QuickCheck              (property)
+
+import           Blockchain.Block             (Block (..), genesisBlock,
+                                               mineBlock)
+import           Blockchain.Block.Header      (Header (..))
+import           Blockchain.Block.Transaction (Transaction (..))
+
+blockHashToString :: Block -> String
+blockHashToString (Block (Header _ _ h) _) = unpack h
 
 spec :: Spec
-spec = undefined
+spec =
+  describe "Blockchain.Block" $ do
+    it "Static check hash of genesisBlock Nothing" $ -- do
+      blockHashToString (genesisBlock Nothing) `shouldBe`
+      "T\240Z\135\245\184\129x\f\220@\227\253\223\235\247.;\167\229\246T\ENQ\171\DC2\FS\DEL\"\217\132\154\180"
+    it "Static check hash of mineBlock Nothing on genesisBlock Nothing" $ -- do
+      blockHashToString (mineBlock (genesisBlock Nothing) Nothing) `shouldBe`
+      "\235D\198D\SYN=b>9\206\GS\253\191\201\&2\DEL0\207?6n\238\159\191\210\161\190\ACK\186!\172\159"
+    it "Hash is determinist" $ -- do
+      property $ \f t a ->
+        genesisBlock (Just (Transaction f t a)) ==
+        genesisBlock (Just (Transaction f t a))
+    it "Blocks with different amounts have different hashes" $ -- do
+      property $ \f t a x ->
+        a == x ||
+        genesisBlock (Just (Transaction f t a)) /=
+        genesisBlock (Just (Transaction f t x))
+    it "Blocks with different senders have different hashes" $ -- do
+      property $ \f t a x ->
+        f == x ||
+        genesisBlock (Just (Transaction f t a)) /=
+        genesisBlock (Just (Transaction x t a))
+    it "Blocks with different receivers have different hashes" $ -- do
+      property $ \f t a x ->
+        t == x ||
+        genesisBlock (Just (Transaction f t a)) /=
+        genesisBlock (Just (Transaction f x a))
