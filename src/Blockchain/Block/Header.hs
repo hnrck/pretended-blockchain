52e927d8fc7686319cf79afcd86a64275d226ae3
Henrick Deschamps <henrick.deschamps@gmail.com>
2018-03-06 18:13:59 +0100

    Adding the block header implementation
    - Adding the block header implementation src.
    - Adding the block header tst.
    - Enabling block header test in cabal file.

--- src/Blockchain/Block/Header.hs
+++ src/Blockchain/Block/Header.hs
@@ -10,9 +10,14 @@ This module is used to implement the blockchain's block header in Haskell.
 -}
 module Blockchain.Block.Header
   ( Header(..)
+  , newHeader
+  , setHash
+  , headerContext
+  , genesisHeader
   ) where
 
 import           Data.ByteString       (ByteString)
+import           Data.ByteString.Char8 (pack)
 
 -- | The header, containing the previous hash, timestamp, and current hash,
 -- computed at block creation.
@@ -24,3 +29,24 @@ data Header = Header
 
 -- | Headers are timestamped with logical time.
 type LogicalTime = Int
+
+-- | generate a new header with a generic hash before hash computation.
+newHeader :: ByteString -> LogicalTime -> Header
+newHeader h t = Header h t (pack "")
+
+-- | Set the hash of an header. Hash has to be computed beforehand.
+setHash :: Header -> ByteString -> Header
+setHash (Header h t _) = Header h t
+
+-- | Generate the header's context for hash generation.
+headerContext :: Header -> [ByteString]
+headerContext (Header h t _) = [h, pack $ show t]
+
+-- | Default hash, used for genesis block.
+defaultHash :: ByteString
+defaultHash = pack "00000000000000000000000000000000"
+
+-- | The header of the first blockchain's block. Previous hash is the default
+-- one  and logical time is set to 0.
+genesisHeader :: Header
+genesisHeader = newHeader defaultHash 0
--- tst/unit/HeaderSpec.hs
+++ tst/unit/HeaderSpec.hs
@@ -1,7 +1,15 @@
 module HeaderSpec where
 
-import           Test.Hspec
-import           Test.QuickCheck
+import           Data.ByteString.Char8   (pack, unpack)
+import           Test.Hspec              (Spec (..), describe, it, shouldBe)
+import           Test.QuickCheck         (property)
+
+import           Blockchain.Block.Header (Header (..), genesisHeader,
+                                          headerContext, newHeader, setHash)
 
 spec :: Spec
-spec = undefined
+spec =
+  describe "Blockchain.Block.Header" $ -- do
+  it "Static check genesis header" $ -- do
+  genesisHeader `shouldBe`
+  Header (pack "00000000000000000000000000000000") 0 (pack "")
