1a079007f7778f31f35cc8a9f58591f05bc3181b
Henrick Deschamps <henrick.deschamps@gmail.com>
2018-03-11 10:52:12 +0100

    Adding transactions to the project
    - Adding the Transactions type.
    - Adding and exporting function to use the transactions in blocks.
    - Adding tests for transactions.

--- src/Blockchain/Block/Transaction.hs
+++ src/Blockchain/Block/Transaction.hs
@@ -11,12 +11,18 @@ blockchain's block, in Haskell.
 -}
 module Blockchain.Block.Transaction
   ( Transaction(..)
+  , Transactions
+  , addTransaction
   , transactionContext
+  , transactionsContext
   ) where
 
 import           Data.ByteString       (ByteString)
 import           Data.ByteString.Char8 (pack)
 import           Data.Maybe            (Maybe (..))
+
+-- | Transactions is a list of transction, potentially empty.
+type Transactions = [Transaction]
 
 -- | A transaction is a contract from someone to someone else, of a certain
 -- amount.
@@ -26,7 +32,17 @@ data Transaction = Transaction
   , amount :: Float
   } deriving (Show, Eq)
 
+-- | Add a transaction to transactions
+addTransaction :: Transaction -> Transactions -> Transactions
+addTransaction t ts = t : ts
+
 -- | Generate the transaction context for hash.
+-- | TODO(hnrck) Removing the Maybe when no more used.
 transactionContext :: Maybe Transaction -> [ByteString]
 transactionContext Nothing = []
 transactionContext (Just (Transaction f t a)) = [pack f, pack t, pack $ show a]
+
+-- | Generate the transactions context for hash.
+transactionsContext :: Transactions -> [ByteString]
+transactionsContext []  = []
+transactionsContext txs = foldr ((++) . transactionContext . Just) [] txs
--- tst/unit/TransactionSpec.hs
+++ tst/unit/TransactionSpec.hs
@@ -5,17 +5,32 @@ import           Test.Hspec                   (Spec (..), describe, it,
                                                shouldBe)
 import           Test.QuickCheck              (property)
 
-import           Blockchain.Block.Transaction (Transaction (..),
-                                               transactionContext)
+import           Blockchain.Block.Transaction (Transaction (..), Transactions,
+                                               addTransaction,
+                                               transactionContext,
+                                               transactionsContext)
 
 spec :: Spec
 spec =
   describe "Blockchain.Block.Transaction" $ do
     it "Static check transaction context of nothing" $ -- do
       transactionContext Nothing `shouldBe` []
+    it "Empty transactions context is empty" $ -- do
+      transactionsContext [] `shouldBe` []
     it "Transaction context is determinist (modulo String packing)" $ -- do
       property $ \f t a ->
         (f /= unpack (pack f)) ||
         (t /= unpack (pack t)) ||
         fmap unpack (transactionContext (Just (Transaction f t a))) ==
         [f, t, show a]
+    it "Adding a transaction to empty transactions makes it not empty" $ -- do
+      property $ \f t a -> addTransaction (Transaction f t a) [] /= []
+    it
+      "Transactions context of two transactions is the concatenation of their contexts" $ -- do
+      property $ \f1 f2 t1 t2 a1 a2 ->
+        transactionsContext
+          (addTransaction
+             (Transaction f1 t1 a1)
+             (addTransaction (Transaction f2 t2 a2) [])) ==
+        transactionContext (Just (Transaction f1 t1 a1)) ++
+        transactionContext (Just (Transaction f2 t2 a2))
